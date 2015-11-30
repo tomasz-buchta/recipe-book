2cc1b56e682a891434a9d58120facb345b70c322
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-11-30 20:23:35 +0100

    Simple schema

---
+++ collections/Recipes.js
@@ -0,0 +1,29 @@
+Recipes = new Meteor.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name"
+  },
+
+  description: {
+    type: String,
+    label: "Description"
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function(){
+      return this.userId
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "Created at",
+    autoValue: function(){
+      return new Date();
+    }
+  }
+});
+
+Recipes.attachSchema( RecipeSchema );
