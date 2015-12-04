2c1476c5b422d02608462c56aa048f499c166e56
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-12-04 17:06:48 +0100

    Add/Remove from menu buttons

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -3,6 +3,9 @@ Recipes = new Mongo.Collection('recipes');
 Recipes.allow({
   insert: function(userId, doc){
     return !!userId;
+  },
+  update: function(userId,doc){
+    return !!userId;
   }
 });
 
@@ -55,6 +58,14 @@ RecipeSchema = new SimpleSchema({
     autoform: {
       type: "hidden"
     }
+  }
+});
+
+Meteor.methods({
+  toggleMenuItem: function(id, currentState){
+    Recipes.update({_id: id}, {$set:{
+      inMenu: !currentState
+    }});
   }
 });
 
